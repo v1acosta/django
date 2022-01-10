5ad06a1f986258a2cbf0d959fed5ef2a330a6821
VictorAcosta <victoracosta@ReDa.local>
2022-01-10 09:11:51 -0600

    first test django

---
+++ Academico/models.py
@@ -0,0 +1,11 @@
+from django.db import models
+
+# Create your models here.
+
+class Curso(models.Model):
+    codigo = models.CharField(primary_key=True, max_length=6)
+    nombre = models.CharField(max_length=50)
+    creditos = models.PositiveSmallIntegerField()
+
+    def __str__(self) -> str:
+        return f'{self.nombre} ({self.creditos})'
